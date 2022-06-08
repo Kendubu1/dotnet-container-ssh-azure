4003e0656fc89e483ffcc61ba8f2e6ae48541cd2
Kendrick <Kendubu1@users.noreply.github.com>
2022-06-07 19:11:51 -0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace howdy
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello 27!");
+                    await context.Response.WriteAsync("Hello 29!");
                 });
             });
         }
